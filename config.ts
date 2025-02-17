5c4e31ba04db6cc6f7d857fdd8f69421d1d473e1
Connor Griffith <congriff@udel.edu>
2025-02-17 13:10:17 -0500

    Initial config file changes

--- config.ts
+++ config.ts
@@ -1,1 +1,79 @@
+export const BOT = {
+	TOKEN: '6031f41b3b4ef9c8f78512b3b33fd643df18fa66b1a35d35fdf46dcaaee994cd', // Bot token here
+	CLIENT_ID: '1340766854427181056', // Client ID here
+	NAME: 'cgrif-Sage'// Bot Name. NEEDS TO BE LESS THAN 11 CHARACTERS
+};
 
+export const MONGO = '';
+
+export const DB = {
+	CONNECTION: 'mongodb+srv://cgriffith:ManchesterUnited25@cluster0.3ps8b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', // Mongo connection string here
+	USERS: 'users',
+	PVQ: 'pvQuestions',
+	QTAGS: 'questionTags',
+	ASSIGNABLE: 'assignable',
+	COURSES: 'courses',
+	REMINDERS: 'reminders',
+	CLIENT_DATA: 'clientData',
+	POLLS: 'polls',
+	JOB_FORMS: 'jobForms'
+};
+
+export const GUILDS = { // Guild IDs for each guild
+	MAIN: '1339665566373384233',
+	GATEWAY: '1339665566373384233',
+	GATEWAY_INVITE: '1339665566373384233'
+};
+
+export const ROLES = { // Role IDS for each role
+	ADMIN: '',
+	STUDENT_ADMIN: '',
+	STAFF: '',
+	VERIFIED: '',
+	MUTED: '',
+	LEVEL_ONE: ''
+};
+
+export const EMAIL = {
+	SENDER: '', // The email address all emails should be sent from
+	REPLY_TO: '', // The replyto address for all emails
+	REPORT_ADDRESSES: [ // A list of all the email address to get the weekly report
+		'' // Add your email here
+	]
+};
+
+export const CHANNELS = { // Channel IDs
+	ERROR_LOG: '1339670678214934679',
+	SERVER_LOG: '1339670734795964578',
+	MEMBER_LOG: '1339670791855538227',
+	MOD_LOG: '1339670814957764751',
+	FEEDBACK: '1339670947325546628',
+	SAGE: '1339670961951215678',
+	ANNOUNCEMENTS: '1339670980087251065',
+	ARCHIVE: '1339670989709115524',
+	ROLE_SELECT: '1339671002170265650'
+};
+
+export const ROLE_DROPDOWNS = {
+	COURSE_ROLES: '',
+	ASSIGN_ROLES: ''
+};
+
+export const LEVEL_TIER_ROLES = [
+	'',
+	'',
+	'',
+	'',
+	''
+];
+
+export const FIRST_LEVEL = 10;
+export const GITHUB_TOKEN = '';
+export const GITHUB_PROJECT = '';
+export const PREFIX = 's;';
+export const MAINTAINERS = 'Connor Griffith';// The current maintainers of this bot
+export const SEMESTER_ID = 's25';// The current semester ID. i.e. s21
+export const BLACKLIST = [];
+
+export const APP_ID = ''; // Adzuna API app ID
+export const APP_KEY = ''; // Adzuna API key
